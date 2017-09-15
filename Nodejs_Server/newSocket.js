b3fd3867b87ef1f28dbc08b470e2fcbbdff17cd7
zzes <kuanglong0312@sina.com>
2017-09-15 10:34:30 +0800

    socket 将redis的数据推到前台

--- Nodejs_Server/newSocket.js
+++ Nodejs_Server/newSocket.js
@@ -12,11 +12,23 @@ app.listen(5000)
 redisClient.on('ready', function() {
     io.on('connection', function(socket) {
         setInterval(function(){
-			redisClient.get("hum", function(err, reply) {
-                console.log("get tem error:" + err)
-                console.log("get the humdata: " + reply)
-                socket.emit('news', { reply })
-            })
+    			redisClient.get("hum", function(err, reply) {
+            console.log("get hum error:" + err)
+            console.log("get the humdata: " + reply)
+            socket.emit('news', { reply })
+          })
+          redisClient.get("tem",function(err, reply){
+            console.log("get tem error:"+err)
+            socket.emit('news', { reply })
+          })
+          redisClient.get("indoor",function(err, reply){
+            console.log("get indoor error:"+err)
+            socket.emit('news', { reply })
+          })
+          redisClient.get("feng",function(err, reply){
+            console.log("get feng error:"+err)
+            socket.emit('news', { reply })
+          })
         }, 10000)
     })
 
