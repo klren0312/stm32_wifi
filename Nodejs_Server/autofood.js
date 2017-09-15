ca46640f4c39e29386f7a8332f6d2c5d0542bd7e
zzes <kuanglong0312@sina.com>
2017-09-15 10:32:59 +0800

    自动喂食失败版

--- Nodejs_Server/autofood.js
+++ Nodejs_Server/autofood.js
@@ -11,14 +11,11 @@ var conn = mysql.createConnection({
 
 conn.connect();
 
-conn.query('select * from autofood',function(err,rows,fields){
-	var i = rows.length;
-	var tem = rows[i-1].type;
-	console.log(tem);
-	switch(tem){
+function change(x){
+	switch(x){
 		case 1:
 			console.log("type1");
-			var j =schedule.scheduleJob('12 * * * *',function(){
+			var j =schedule.scheduleJob('feed','12 * * * *',function(){
 				var post = {status:1};
 				conn.query('INSERT INTO status SET ?', post ,function(error,result,fields){
 					if(error) throw error;
@@ -27,13 +24,13 @@ conn.query('select * from autofood',function(err,rows,fields){
 			break;
 		case 2:
 			console.log("type2");
-			var j =schedule.scheduleJob('9 * * * *',function(){
+			var j =schedule.scheduleJob('feed','9 * * * *',function(){
 				var post = {status:1};
 				conn.query('INSERT INTO status SET ?', post ,function(error,result,fields){
 					if(error) throw error;
 				});
 			});
-			var k =schedule.scheduleJob('15 * * * *',function(){
+			var k =schedule.scheduleJob('feed','15 * * * *',function(){
 				var post = {status:1};
 				conn.query('INSERT INTO status SET ?', post ,function(error,result,fields){
 					if(error) throw error;
@@ -42,19 +39,19 @@ conn.query('select * from autofood',function(err,rows,fields){
 			break;
 		case 3:
 			console.log("type3");
-			var j =schedule.scheduleJob('8 * * * *',function(){
+			var j =schedule.scheduleJob('feed','8 * * * *',function(){
 				var post = {status:1};
 				conn.query('INSERT INTO status SET ?', post ,function(error,result,fields){
 					if(error) throw error;
 				});
 			});
-			var k =schedule.scheduleJob('12 * * * *',function(){
+			var k =schedule.scheduleJob('feed','12 * * * *',function(){
 				var post = {status:1};
 				conn.query('INSERT INTO status SET ?', post ,function(error,result,fields){
 					if(error) throw error;
 				});
 			});
-			var l =schedule.scheduleJob('18 * * * *',function(){
+			var l =schedule.scheduleJob('feed','18 * * * *',function(){
 				var post = {status:1};
 				conn.query('INSERT INTO status SET ?', post ,function(error,result,fields){
 					if(error) throw error;
@@ -65,4 +62,37 @@ conn.query('select * from autofood',function(err,rows,fields){
 			console.log("error");
 			break;
 	}
-})
+}
+
+function check(x){
+	conn.query('select * from autofood', function(err,rows,fields){
+				let len = rows.length;
+				let tem = rows[len-1].type;
+				if (tem !== x) {
+					if (schedule.scheduledJobs['feed']) {
+						var result = schedule.cancelJob('feed');
+						console.log("关闭当前定时: ")
+						console.log(result)
+					}
+					big()
+				}else{
+					console.log("same")
+				}
+			})
+}
+ 
+
+function big(){
+	conn.query('select * from autofood',function(err,rows,fields){
+		var i = rows.length;
+		var tem = rows[i-1].type;
+		console.log(tem);
+		change(tem)
+		console.log(schedule.scheduledJobs)
+		setInterval(function(){
+			check(tem)
+		},3000)
+	})
+}
+
+big();
